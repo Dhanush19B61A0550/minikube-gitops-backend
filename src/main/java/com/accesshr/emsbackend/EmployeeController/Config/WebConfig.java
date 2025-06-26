3e96502db40dd48d69bd7779f52ca1cd99a04142
Dhana Sekhar <dhanushbunny8@gmail.com>
2025-06-26 15:43:14 +0530

    A

--- src/main/java/com/accesshr/emsbackend/EmployeeController/Config/WebConfig.java
+++ src/main/java/com/accesshr/emsbackend/EmployeeController/Config/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Adjust the mapping as needed
+        registry.addMapping("/**") // Adjust the mapping as needed
                 .allowedOrigins("http://localhost:3000","https://fine-entry-459905-s5.uc.r.appspot.com", "http://localhost:5173") // Your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
                 .allowedHeaders("*") // Allow all headers
